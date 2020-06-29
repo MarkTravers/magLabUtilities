7127ba7877c19beea0fd3e5db1699a7430418f54
Mark Travers <marktheshark823@gmail.com>
2020-06-29 16:23:18 -0600

    Generating XExp with M data works

--- tests/workflowTests/dataModelCalcCost.py
+++ tests/workflowTests/dataModelCalcCost.py
@@ -7,14 +7,34 @@ from magLabUtilities.signalutilities.hysteresis import XExpQA, HysteresisSignalB
 from magLabUtilities.uiutilities.plotting.hysteresis import MofHPlotter, XofMPlotter
 
 if __name__=='__main__':
-    # fp = './tests/workflowTests/datafiles/M(H)_Curves_Netza.xlsx'
-    # refBundle = HysteresisSignalBundle(importFromXlsx(fp, '21k', 2, 'C,D', dataColumnNames=['M','H']))
+    fp = './tests/workflowTests/datafiles/test21kLoop.xlsx'
+    refBundle = HysteresisSignalBundle(importFromXlsx(fp, '21k', 2, 'C,D', dataColumnNames=['H','M']))
 
-    # xExpQA = XExpQA(xInit=67.0, hCoercive=630.0, mSat=1.67e6, hCoop=1200.0, hAnh=2000.0, xcPow=4.0, mRev=-11.0)
-    # testBundle = HysteresisSignalBundle(xExpQA.evaluate(refBundle.signals['M']))
+    mMatrix = refBundle.signals['M'].independentThread.data
+    tMatrix = refBundle.signals['M'].dependentThread.data
+    pMAmpIndex = np.argmax(mMatrix[0:int(mMatrix.shape[0]/2)])
+    nMAmpIndex = np.argmin(mMatrix)
 
-    # mhPlotter = MofHPlotter([(refBundle, 'Ref0')])
+    virginGen = XExpQA(xInit=67.0, hCoercive=630.0, mSat=1.67e6, hCoop=1200.0, hAnh=3000.0, xcPow=4.0, mRev=0.0)
+    pRevGen = XExpQA(xInit=67.0, hCoercive=630.0, mSat=1.67e6, hCoop=1200.0, hAnh=3000.0, xcPow=4.0, mRev=mMatrix[pMAmpIndex])
+    nRevGen = XExpQA(xInit=67.0, hCoercive=630.0, mSat=1.67e6, hCoop=1200.0, hAnh=3000.0, xcPow=4.0, mRev=mMatrix[nMAmpIndex])
 
-    # xmPlotter = XofMPlotter([(testBundle, 'Test0')])
+    virginM = Signal.fromThreadPair(SignalThread(mMatrix[0:pMAmpIndex]), SignalThread(tMatrix[0:pMAmpIndex]))
+    virginX = virginGen.evaluate(mSignal=virginM)
 
+    pRevM = Signal.fromThreadPair(SignalThread(mMatrix[pMAmpIndex:nMAmpIndex]), SignalThread(tMatrix[pMAmpIndex:nMAmpIndex]))
+    pRevX = pRevGen.evaluate(mSignal=pRevM)
+    
+    nRevM = Signal.fromThreadPair(SignalThread(mMatrix[nMAmpIndex:]), SignalThread(tMatrix[nMAmpIndex:]))
+    nRevX = nRevGen.evaluate(mSignal=nRevM)
+
+    testBundle = HysteresisSignalBundle.fromSignalBundleSequence([virginX, pRevX, nRevX])
+
+    plotter = XofMPlotter()
+    # plotter.addPlot(refBundle, 'Data')
+    plotter.addPlot(virginX, 'Virgin')
+    plotter.addPlot(pRevX, 'Positive Reversal')
+    plotter.addPlot(nRevX, 'Negative Reversal')
+
+    input('Press Return to exit...')
     print('done')
