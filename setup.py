26b05569c6245e72eb6af3b43b77b8f2e061ce20
Mark Travers <marktheshark823@gmail.com>
2021-01-13 12:31:27 -0700

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='magLabUtilities',
-    version='0.1.0',
+    version='1.0.0',
     author='Mark Travers',
     author_email='mark.2.travers@ucdenver.edu',
     description='Commonly used utilities in the UC Denver magnetics lab.',
