7027094b6b71bc540d7a1dee1944bb277e409385
Mark Travers <marktheshark823@gmail.com>
2021-01-13 14:54:38 -0700

    release 1.0.1; changing dependency configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='magLabUtilities',
-    version='1.0.0',
+    version='1.0.1',
     author='Mark Travers',
     author_email='mark.2.travers@ucdenver.edu',
     description='Commonly used utilities in the UC Denver magnetics lab.',
@@ -15,7 +15,22 @@ setuptools.setup(
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=[
-        'numpy>=1.18.3'
+        'numpy==1.18.3'
+        'astroid==2.4.2'
+        'colorama==0.4.4'
+        'dill==0.3.3'
+        'isort==5.6.4'
+        'lazy-object-proxy==1.4.3'
+        'mccabe==0.6.1'
+        'multiprocess==0.70.11.1'
+        'pathos==0.2.7'
+        'pox==0.2.9'
+        'ppft==1.6.6.3'
+        'pylint==2.6.0'
+        'six==1.15.0'
+        'toml==0.10.2'
+        'typed-ast==1.4.1'
+        'wrapt==1.12.1'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
